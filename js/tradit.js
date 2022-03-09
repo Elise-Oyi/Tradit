ae8dcb0fb36cf73f7405cad78f7560cf4acae88c
Elise-Oyi <maryeliseangbo@gmail.com>
2022-03-09 15:31:15 +0100

    first commit

---
+++ js/tradit.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', function(){
+    
+    links.classList.toggle("nav-links")
+})
